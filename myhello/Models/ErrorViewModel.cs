8e6c9ae7b52b184491ade9cc7b08c5ecee5e5668
samuelh <samuelh@b.com>
2020-05-28 12:10:02 +0100

    Add source code for myhello

---
+++ myhello/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace myhello.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
